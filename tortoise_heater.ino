9d541aea4f02557685cfdae3a48ac4d6b42a7d1c
Mick04 <mac5y4@talktalk.net>
2023-02-27 15:16:16 +0000

    OTA added

--- tortoise_heater.ino
+++ tortoise_heater.ino
@@ -2,18 +2,17 @@
 
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
-#include "LittleFS.h"
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <NTPClient.h>  //
 #include <PubSubClient.h>
 #include <ESP8266WebServer.h>
 #include <Adafruit_Sensor.h>
-#include <WiFiUdp.h>
-//#include <ESP8266WebServer.h>
 #include <OneWire.h>
-//#include <iostream>
-//#include <string.h> branch 
-//#include <stdio.h>
-//#include <cstdint>
+#include "LittleFS.h"
+
 #define Relay_Pin D5  //active board
 #define LED_Pin 13    //on board LED_Pin
 //#define LED_Pin D6//LED_Pin  //change when debuged
@@ -96,6 +95,21 @@ long int value = 0;
 /********************************************
  *    wifi and pubSup credentials end       *
  * ******************************************/
+ /********************************************
+   Static IP address and wifi conection Start
+ ********************************************/
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 184);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
+/********************************************
+     Static IP address and wifi conection end
+ ********************************************/
 
 void setup_wifi() {
 
@@ -654,10 +668,50 @@ Serial.print("** 476 ** s1 = ");
 
     return;
   }
+  
+  /************************************
+ *       OVER THE AIR START         *        
+ *                                  *
+ ************************************/   
 
-}    
+    //ArduinoOTA.setHostname("INSIDE");
+    ArduinoOTA.setHostname("TEMPLATE");
+    //ArduinoOTA.setHostname("TEST RIG");
+    ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
 
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  }
+/************************************
+ *       OVER THE AIR END           *
+ ************************************/  
   void loop() {
+    ArduinoOTA.handle();
 //delay(1000);
     if (!client.connected()) {
       reconnect();
